e9b884c45ff1378b8f88734551466446dbf47791
Jodie Hoh <jhoh5@jhu.edu>
2021-05-10 00:01:52 -0400

    why doesnt it work :,)

--- website/population_history.php
+++ website/population_history.php
@@ -14,8 +14,6 @@ if (isset($_POST['country'])) {
     $country = $_POST['country'];
 }
 
-//Collect the posted value in a variable called $item
-$country = $_POST['country'];
 
 echo "<h2>Bid History</h2>";
 echo "Item number: ";
