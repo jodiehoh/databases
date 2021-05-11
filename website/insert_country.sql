8fb5d9f9c7b91d9fd45ec4abaee6fec9be2803c7
Aditi Sen <aditisen@Aditis-MacBook-Pro.local>
2021-05-11 17:27:48 -0400

    insert country stuff

--- website/insert_country.sql
+++ website/insert_country.sql
@@ -5,7 +5,6 @@ DROP PROCEDURE IF EXISTS InsertCountry //
 CREATE PROCEDURE InsertCountry(IN conti VARCHAR(255),
 	IN cntry VARCHAR(255),
 	IN govt VARCHAR(255),
-	IN cntryCode VARCHAR(30),
 	IN rgn VARCHAR(255),
 	IN incomeType VARCHAR(255))
 
@@ -14,7 +13,7 @@ BEGIN
 	IF NOT EXISTS(SELECT * FROM Country WHERE CountryName = cntry) THEN	       
 		
 		INSERT INTO Country (Continent, CountryName, Government, CountryCode, Region, IncomeGroup) 
-		VALUES (conti, cntry, govt, cntryCode, rgn, incomeType);
+		VALUES (conti, cntry, govt, rgn, incomeType);
 	
 	END IF;
 
