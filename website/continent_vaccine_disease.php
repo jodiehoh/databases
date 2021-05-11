0e78ae2eaadd53b5a1523b4a6033e068dd85bd83
Jodie Hoh <jhoh5@jhu.edu>
2021-05-10 22:21:21 -0700

    hewwo

--- website/continent_vaccine_disease.php
+++ website/continent_vaccine_disease.php
@@ -49,33 +49,33 @@ if (false) {
         //Report result set by visiting each row in it
                while ($row = $result->fetch_row()) {
                   $vaccine = NULL;
-                  $vaccine->x = $row[0];
-                  $vaccine->y = $row[2];
+                  $vaccine->label = $row[0];
+                  $vaccine->y = intval($row[2]);
 
                   array_push($vaccines, $vaccine);
                   
                   $disease = NULL;
-                  $disease->x = $row[0];
-                  $disease->y = $row[1];
+                  $disease->label = $row[0];
+                  $disease->y = intval($row[1]);
 
                   array_push($diseases, $disease);
             } 
 
 
                $outer = NULL;
-	      $outer->type = "line";
-	      $outer->axisYType= "secondary";
-	      $outer->name = "Vaccine Doses";
-	      $outer->markerSize = 0;
+      	      $outer->type = "line";
+      	      $outer->axisYType= "secondary";
+      	      $outer->name = "Vaccine Doses";
+      	      $outer->markerSize = 0;
                $outer->showInLegend = "true";
                $outer->dataPoints = $vaccines;
 
                array_push($data, $outer);
 
                $outer = NULL;
-	       $outer->type = "line";
-	       $outer->axisYType= "secondary";
-	       $outer->markerSize = 0;
+	             $outer->type = "line";
+	             $outer->axisYType= "secondary";
+	             $outer->markerSize = 0;
                $outer->name = "Disease Cases";
                $outer->showInLegend = "true";
                $outer->dataPoints = $diseases;
